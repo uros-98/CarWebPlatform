691cae2f2dc5a2369192a43bf635a406de6be6d8
Uros <uros.stanojevic.27@gmail.com>
2025-11-18 19:11:03 +0100

    Rename LoginRequest to LoginRequestDTO; Change class LoginRequestDTO to record

--- src/main/java/com/asss/zavrsni/rad/dto/LoginRequestDTO.java
+++ src/main/java/com/asss/zavrsni/rad/dto/LoginRequestDTO.java
@@ -1,7 +1,17 @@
 package com.asss.zavrsni.rad.dto;
 
-public record LoginRequestDTO (
-        String username,
-        String password
-    ) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+
+    private String username;
+    private String password;
+
 }
