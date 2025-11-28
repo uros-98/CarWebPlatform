2eb51cba38f6d6204cba4d9f4c6d87652235f8de
Uros <uros.stanojevic.27@gmail.com>
2025-11-29 00:07:49 +0100

    Implemented permission for admin/users

--- src/main/java/com/asss/zavrsni/rad/repository/UserRepository.java
+++ src/main/java/com/asss/zavrsni/rad/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.asss.zavrsni.rad.repository;
 import com.asss.zavrsni.rad.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
--- src/main/java/com/asss/zavrsni/rad/service/UserService.java
+++ src/main/java/com/asss/zavrsni/rad/service/UserService.java
@@ -5,7 +5,6 @@ import com.asss.zavrsni.rad.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
